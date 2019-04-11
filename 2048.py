0f127368d0174f37d36d8cacc99cdf9ee1874c61
ainur <ainureg@ya.ru>
2019-04-11 18:31:57 +0300

    nothing particularly

--- 2048.py
+++ 2048.py
@@ -1,69 +1,27 @@
-#import pandas as pd
-#import numpy as np
-#import re
 from pynput.keyboard import Listener, Key, Controller
 import os
-#os.chdir('/home/ainur/Desktop/Codes/Python/problems/2048/')
-
 #from datetime import datetime
 #import functions as f
 from time import sleep
 from functions import gotmove, mv
 from classes import tbl
-#t=tbl()
-#t.load()
-
-
 from classes import score
-#sc=score()
-#sc.load()
-#sc.predict()
-#sc.score
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#just showing picture of an array
-
-
-
-
-
-
-global globvar
+#global globvar
 
 
 def on_release(key):
     global globvar
     if key == Key.esc:
 ##        Stop listener
-        globvar=False
+#        globvar=False
 #        from sys import exit
 #        exit()
         return False
     
 def on_press(key):
     global t
-    global globvar
+#    global globvar
     if key == Key.enter:
 #        while globvar:
         k=gotmove(t.tbl)
