15af67293d6b614d51b88745e1495fa6bf8bc1c3
ainur <ainureg@ya.ru>
2019-04-12 19:33:03 +0300

    added state

--- 2048.py
+++ 2048.py
@@ -1,11 +1,13 @@
 from pynput.keyboard import Listener, Key, Controller
 import os
-#from datetime import datetime
-#import functions as f
 from time import sleep
-from functions import gotmove, mv
+from functions import mv, mem#,gotmove 
 from classes import tbl
 from classes import score
+
+from mss import mss
+import mss.tools as msstools
+from datetime import datetime
 
 #global globvar
 
@@ -14,26 +16,75 @@ def on_release(key):
     global globvar
     if key == Key.esc:
 ##        Stop listener
-#        globvar=False
+        globvar=False
 #        from sys import exit
 #        exit()
         return False
     
 def on_press(key):
     global t
-#    global globvar
     if key == Key.enter:
 #        while globvar:
-        k=gotmove(t.tbl)
-        mv(k, 0)
-        t.gettbl(ts=0)
-        print(k,'\n___________________\n')
 #    if key == Key.esc:
 #        return False      
-    
+        
+        try:
+            while True:
+                k=mem(t.tbl)
+                mv(k, 0)
+                t.gettbl(ts=0)
+                #print(k,'\n___________________\n')
+        except:
+            
+            with mss() as sct:
+                sleep(10)
+                l1,l2 =107, 15
+                top, left =335, 381
+                monitor = {"top": top, "left": left, \
+"width": 4*l1+5*l2, "height": 4*l1+5*l2}
+                
+                img = str(datetime.now().time())+".png".format(**monitor)
+                sct_img = sct.grab(monitor)
+                msstools.to_png(sct_img.rgb, sct_img.size, output='./data/gamestate/lost/'+img)
+                keyboard.press(Key.f5)
+                keyboard.release(Key.f5)
+                print('АХАХАХХ проиграли')
+                sleep(10)
+                t.gettbl(ts=0)
+                on_press(Key.enter)
+            
     return True
 
 
+
+#CPU times: user 377 ms, sys: 2.96 ms, total: 380 ms
+#Wall time: 369 ms
+#Out[56]: <Key.up: <65362>>
+
+#def mem1(M):
+#    keys=[Key.right, Key.down, Key.up, Key.left]    
+#    cols=['k1','k2','s','nanc', 's0']
+#    solutions=DataFrame(columns=cols)
+#    for k1,k2 in product(keys,keys):
+#        temp=M.copy(deep=True)
+#        temp2=nt(temp,k1).apply(np.log2)
+#        temp=nt(nt(temp,k1),k2)
+#        temp=temp.apply(np.log2)
+#        stemp=temp.sum().sum()
+#        temp2=temp2.sum().sum()
+#        counttemp=nancount(temp)
+#        solutions=solutions.append(DataFrame([[k1,k2,stemp,counttemp, temp2]],
+#                                             columns=cols),ignore_index=True)
+#
+#    
+#    solutions=solutions.loc[ solutions.s==solutions.s.min(),:]
+#    solutions=solutions.loc[ solutions.nanc==solutions.nanc.min(),:]
+#    solutions.loc[solutions.s0==solutions.s0.min(),:]
+#    k1=solutions.iloc[0,0]
+#
+#    return(k1)
+    
+    
 
 
 
@@ -43,18 +94,21 @@ if __name__ == "__main__":
     sc.load()
     t=tbl()
     t.load()
-    t.gettbl()
-    sleep(3)
+    t.gettbl(ts=0)
+#    sleep(3)
     globvar=True
     keyboard = Controller()
     with Listener(
             on_press=on_press,
             on_release=on_release) as listener:
+        
+        
         print('still running...')
-        while globvar:
-            sleep(0.1)
-            if not listener.running:
-                break
+        listener.join()
+#        while globvar:
+#            sleep(0.1)
+#            if not listener.running:
+#                break
         
 
 
--- classes.py
+++ classes.py
@@ -1,16 +1,3 @@
-
-
-class tbl():
-    
-    def __init__(self):
-        123
-        
-
-    def window2(self): 
-        345                                            # <===
-        
-
-
 import cv2, re
 import numpy as np
 from sklearn.linear_model import SGDClassifier
@@ -19,12 +6,10 @@ from time import sleep
 from pandas import DataFrame
 from functions import scrn, getscore, getld
 import os
-import mss
-
-try:
-    from PIL import Image
-except ImportError:
-    import Image
+from mss import mss
+import mss.tools as msstools
+from cv2 import imread, split
+from datetime import datetime
 
 
 class tbl():
@@ -32,7 +17,7 @@ class tbl():
         ar=[]
         Y=[]
         for fl in flist:
-            temp=cv2.imread(fl,0).reshape(-1,)
+            temp=imread(fl,0).reshape(-1,)
             ar.append(temp)
             Y.append(re.search( '/.*/(.*)/', fl).group(1) )
         ar=np.array(ar)
@@ -50,7 +35,7 @@ class tbl():
           
     def getl(self):
         #find params for images
-        with mss.mss() as sct:
+        with mss() as sct:
             sleep(3)
             test = sct.shot(output='fullscreen.png')
         pr=cv2.imread(test,0) 
@@ -58,46 +43,47 @@ class tbl():
         
         
     def gettbl(self,ts=4, save=False):
-        import mss.tools
-        import mss
-        from datetime import datetime
         self.tbl=DataFrame(columns=np.arange(4), index=np.arange(4))
         sleep(ts)
         l1=107
         l2=15
         top=335
-#        left=556
         left=381
-        with mss.mss() as sct:
+        with mss() as sct:
             for i in range(4):
                 for j in range(4):
                     monitor = {"top": top+l2+i*(l1+l2), "left": left+l2+j*(l1+l2), \
                    "width": l1, "height": l1}
-                    test = str(i)+'*'+str(j)+str(datetime.now().time())+".png".format(**monitor)
-                    # Grab the data
-                    sct_img = sct.grab(monitor)
-                    # Save to the picture file
-                    mss.tools.to_png(sct_img.rgb, sct_img.size, output=test)
-                    pr=cv2.imread(test,0) 
-                    if not save:
-                        os.remove(test)
-                    pr=self.sgd.predict(pr.reshape(1,-1) )[0]
-                    if pr=='np.nan':
+                    
+#                    n=split(np.array(sct.grab(monitor)) )[0]
+                    
+                    #both work and nice                    
+                    n = np.array(sct.grab(monitor))
+                    n=cv2.cvtColor( n, cv2.COLOR_BGR2GRAY)
+
+                    
+                    n=self.sgd.predict(n.reshape(1,-1) )[0]
+                    if n=='np.nan':
                         self.tbl.iloc[i,j]=np.nan
                     else:
-                        self.tbl.iloc[i,j]=float(pr)
+                        self.tbl.iloc[i,j]=float(n)
+                    if save:
+
+                        img = str(i)+'*'+str(j)+str(datetime.now().time())+".png".format(**monitor)
+                        sct_img = sct.grab(monitor)
+                        msstools.to_png(sct_img.rgb, sct_img.size, output=img)
+                        pr=imread(img,0) 
         self.tbl=self.tbl.astype(float)
         
         
         
         
 class score():
-    
     def fit(self,flist):
         ar=[]
         Y=[]
         for fl in flist:
-            temp=cv2.imread(fl,0).reshape(-1,)
+            temp=imread(fl,0).reshape(-1,)
             ar.append(temp)
             Y.append(re.search( '/.*/(.*)/', fl).group(1) )
         ar=np.array(ar)
@@ -122,14 +108,14 @@ class score():
             temp=np.concatenate( (j, np.repeat( [colorcol], maxlength-len(j) , axis=1)[0]))   
             temp=np.reshape(f.arflat(temp), [-1,maxlength,3], order='F')
             Image.fromarray(temp).convert("RGB").save('temp.png')
-            temp=cv2.imread('temp.png',0)
+            temp=imread('temp.png',0)
             score=score+(self.sgd.predict(temp.reshape(1,-1))[0])
             os.remove('temp.png')
         self.score=score
           
     def getl(self):
         #find params for images
-        with mss.mss() as sct:
+        with mss() as sct:
             sleep(3)
             test = sct.shot(output='fullscreen.png')
         pr=cv2.imread(test,0) 
@@ -142,7 +128,7 @@ class score():
         for i in range(4):
             for j in range(4):
                 test=scrn(i,j)
-                pr=cv2.imread(test,0) 
+                pr=imread(test,0) 
                 if not save:
                     os.remove(test)
                 pr=self.sgd.predict(pr.reshape(1,-1) )[0]
@@ -152,3 +138,46 @@ class score():
                     self.tbl.iloc[i,j]=float(pr)
         self.tbl=self.tbl.astype(float)
     
+    
+    
+    
+    
+    
+    
+class state():
+    
+    def __init__(self):
+        self.load()
+        
+        
+        
+    def fit(self,flist):
+        ar=[]
+        Y=[]
+        for fl in flist:
+            temp=imread(fl,0).reshape(-1,)
+            ar.append(temp)
+            Y.append(re.search( '/.*/(.*)/', fl).group(1) )
+        ar=np.array(ar)
+            
+        sgd_clf = SGDClassifier(max_iter=500)
+        self.sgd=sgd_clf.fit(ar, Y)
+    def save(self, fname='./.conf/state.joblib'):
+        dump(self.sgd, fname) 
+        
+    def load(self, fname='./.conf/state.joblib'):
+        self.sgd=load(fname) 
+        
+    def predict(self):
+        with mss() as sct:
+            l1,l2 =107, 15
+            top, left =335, 381
+            monitor = {"top": top, "left": left, \
+                       "width": 4*l1+5*l2, "height": 4*l1+5*l2}
+            
+            #n=split(np.array(sct.grab(monitor)) )[0]
+            sc = np.array(sct.grab(monitor))
+            sc=cv2.cvtColor( sc, cv2.COLOR_BGR2GRAY)
+            self.now=self.sgd.predict(sc.reshape(1,-1))
+          
+        
